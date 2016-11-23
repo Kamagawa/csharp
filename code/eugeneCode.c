f08916213ae6a4ac88009c52b84969dfc931cfe5
Eugene Wang <eugene.r.w.12@gmail.com>
2016-11-23 14:18:11 -0500

    started on the autonomous check during piston movement

--- code/eugeneCode.c
+++ code/eugeneCode.c
@@ -1,20 +1,24 @@
-bool movePiston(int power, int distanceCM, int gearRadiusCM, int cm = 0, ){
+bool movePiston(int power, int distanceCM, int gearRadiusCM, int cm = 0 ){
 
 	 	int distPcts = gearRadiusCM * 2 * PI /360;
+	 	int avgSpeed;
+	 	int tempEncoder;
 
 	 	m1.setMotorPower (power);
 	 	m1.setMotorPower (power);
 	 	nMotorEncoder[motorB] = 0;
+	 	clearTimer(T1);
 
 	 	while ( distPcts * nMotorEncoder[motorB] < distance)
 	  {
-	  	//Emergency Stop right here.
+	  	if (time (T1)>=100){
+	  		avgSpeed = nMotorEncoder[motorB];
+	  	}
+	  		//Emergency Stop right here.
 	  }
-
 
    m1.setMotorPower (0);
 	 m1.setMotorPower (0);
 
-   nMotorEncoder[motorB] = 0;
 	 return true;
 }
