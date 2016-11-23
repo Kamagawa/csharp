045b8311fd68e1e95541b3d1a2ad5f364fc98749
Eugene Wang <eugene.r.w.12@gmail.com>
2016-11-23 14:08:21 -0500

    function move piston

--- code/eugeneCode.c
+++ code/eugeneCode.c
@@ -1,5 +1,19 @@
-bool movePiston(int power, int distance, int gearRadius, int cm = 0, ){
-	 int spin_dist = 0;
+bool movePiston(int power, int distanceCM, int gearRadiusCM, int cm = 0, ){
+
+	 	int distPcts = gearRadiusCM * 2 * PI /360;
+
+	 	m1.setMotorPower (power);
+	 	m1.setMotorPower (power);
+	 	nMotorEncoder[motorB] = 0;
+
+	 	while ( distPcts * nMotorEncoder[motorB] < distance)
+	  {
+	  	//Emergency Stop right here.
+	  }
+
+
+   m1.setMotorPower (0);
+	 m1.setMotorPower (0);
 
    nMotorEncoder[motorB] = 0;
 	 return true;
