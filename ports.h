8c811f82d7d35d50e38a0f26dbb298ce77643813
Feilan Jiang <f-jiang@github.com>
2016-11-22 20:43:44 -0500

    Add port mappings for motors and sensors

---
+++ ports.h
@@ -0,0 +1,13 @@
+#ifndef PORTS_H
+#define PORTS_H
+
+#define WHEEL    1
+#define TRAY     2
+#define BELT     3
+
+#define WHEEL_TOUCH         S1
+#define COLOR         S2
+#define ULTRA         S3
+#define TRAY_TOUCH    S4
+
+#endif
