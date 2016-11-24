eeca4cdad16b94e71938d3316e1f521e2fff14f4
Feilan Jiang <f-jiang@github.com>
2016-11-23 19:06:57 -0500

    Add initialization function for sensors

--- code/controls.c
+++ code/controls.c
@@ -1,4 +1,13 @@
 #ifndef CONTROLS_C
 #define CONTROLS_C
 
+#include "ports.c"
+
+void init() {
+    SensorType[WHEEL_TOUCH] = sensorTouch;
+    SensorType[COLOR] = sensorColorNxtFULL;
+    SensorType[ULTRA] = sensorSONAR;
+    SensorType[TRAY_TOUCH] = sensorTouch;
+}
+
 #endif
