b550461b44fac595f5a7538bac4fbcac5cb99ab5
Feilan Jiang <f-jiang@github.com>
2016-11-24 01:31:08 -0500

    Add function outlines to main.c

--- code/main.c
+++ code/main.c
@@ -3,6 +3,33 @@
 #include "nxtToggleBtn.c"
 #include "ports.c"
 
+const int BATTERY_THRESHOLD = 9000;
+const int TRAY_DIST_CM = 30;
+
+// check that the pencil tray is not removed during sharpening
+task monitorTray() {
+	while (nAvgBatteryLevel < BATTERY_THRESHOLD) {
+		if (SensorValue[ULTRA] > TRAY_DIST_CM) {
+			// tray lifted during sharpening
+		}
+	}
+}
+
+task sharpenAndSort() {
+
+}
+
+// display menu dialog to let user pick pencil sorting method before sharpening begins
+task pickSortingMethod() {
+
+}
+
+// display number of pencils of each category after sharpening
+// also includes option to sharpen more pencils
+task displayInventory() {
+
+}
+
 task main() {
 
 }
