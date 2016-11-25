9f5a6a90b021b41a81b7907c5e3fba131f2ea803
Eugene Wang <eugene.r.w.12@gmail.com>
2016-11-25 02:57:23 -0500

    proper doc for controls

--- code/controls.c
+++ code/controls.c
@@ -55,6 +55,22 @@ const int N_BINS = 7;
 const int BIN_DIST = 0;
 
 
+/**
+ * init
+ * {code  void init(); }
+ *
+ * Description:
+ * the init function initiates the sensor values, and set
+ * the correct ports to sensor, hence starting the function
+ * of the program and will be called by taks main ();
+ *
+ * Note:
+ * This is a mid-levl modular function, that facilitate the
+ * system operation, thus it requires little var from outside
+ * environment as it has been handled in lower-level functions
+ *
+ * @return a void task do not return anything.
+*/
 void init() {
 	SensorType[WHEEL_TOUCH] = sensorTouch;
 	SensorType[COLOR] = sensorColorNxtFULL;
@@ -62,6 +78,27 @@ void init() {
 	SensorType[TRAY_TOUCH] = sensorTouch;
 }
 
+
+/**
+ * moveBelt
+ * {code  moveBelt(100, 50); }
+ *
+ * Description:
+ * The move belt function moves the belt with MotorPower power
+ * and the amout of time tMs, if not specified it will keep
+ * moving at setted power.
+ * This is to send the pencils to the feeder.
+ *
+ * @param power thePower of motor moving the belt
+ * @param tMs the time length in milisenconds that the belt will move
+ *
+ * Note:
+ * This is a low-levl modular function, that facilitate the
+ * system operation, thus it requires little var from outside
+ * environment as it has been handled in lower-level functions
+ *
+ * @return a void task do not return anything.
+*/
 void moveBelt(int power, int tMs = -1) {
 	motor[BELT] = power;
 
@@ -72,6 +109,27 @@ void moveBelt(int power, int tMs = -1) {
 	}
 }
 
+
+/**
+ * spinWheels
+ * {code  spinWheels(100, 50); }
+ *
+ * Description:
+ * The spinWheels function spins the wheel with MotorPower power
+ * and the amout of time tMs, if not specified it will keep
+ * moving at setted power.
+ * This is to feed the pencil to the pencil sharpener
+ *
+ * @param power thePower of motor moving the belt
+ * @param tMs the time length in milisenconds that the belt will move
+ *
+ * Note:
+ * This is a low-levl modular function, that facilitate the
+ * system operation, thus it requires little var from outside
+ * environment as it has been handled in lower-level functions
+ *
+ * @return a void task do not return anything.
+*/
 void spinWheels (int power, int tMs = -1)
 {
 	motor[WHEEL] = power;
@@ -83,6 +141,27 @@ void spinWheels (int power, int tMs = -1)
 	}
 }
 
+
+/**
+ * moveTray
+ * {code  moveTray(100, 500.21); }
+ *
+ * Description:
+ * The moveTray function moves the tray with MotorPower power
+ * and the distance in distCm which will be coverted into timing
+ * for robot to calculate.
+ * This is to feed the pencil to the pencil sharpener
+ *
+ * @param power thePower of motor moving the belt
+ * @param distCm the time length in milisenconds that the belt will move
+ *
+ * Note:
+ * This is a low-levl modular function, that facilitate the
+ * system operation, thus it requires little var from outside
+ * environment as it has been handled in lower-level functions
+ *
+ * @return a void task do not return anything.
+*/
 void moveTray (int power, float distCm)
 {
 	int encVal = distCm / CIRCUM_CM * 360;
