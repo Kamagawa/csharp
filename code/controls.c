e8f07411cfa7157ea7249d32c7526b5006dc4bc5
Feilan Jiang <f-jiang@github.com>
2016-11-25 09:49:53 -0500

    Syntax fix

--- code/controls.c
+++ code/controls.c
@@ -6,7 +6,7 @@
 
 const float DIAM_CM = 3.7;
 const float CIRCUM_CM = DIAM_CM * PI;
-const int ENC_TOL = 1
+const int ENC_TOL = 1;
 const int N_BINS = 7;
 const int BIN_DIST = 0;
 const int JAM_THRESHOLD = 10;
