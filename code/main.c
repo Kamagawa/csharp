6f8c43d5915dfcd2ee4854c76014a3eb3a75dc00
Feilan Jiang <f-jiang@github.com>
2016-12-05 01:02:59 -0500

    Revise comments in main.c

--- code/main.c
+++ code/main.c
@@ -1,31 +1,39 @@
-/**  main.c
-* stucture csharp's program
-* 
-* constant variables: BATTERY_THRESHOLD, TRAY_DIST_CM *
-* Funcions:  monitorTray(), sharpenAndSort(), task main()
-*
-* @author: Eugene Wang, Feilan Jiang, Kenta Morris, Felix Cheng
-* @version 1.2
-* @since   2016-11-23 */
+/** main.c
+ * This is the primary file of the Csharp program. Here, the overall control flow of the
+ * program is defined. Each "step" performed by the robot is represented by functions 
+ * defined in the file actions.c.
+ * 
+ * Constants:
+ * const int BATTERY_THRESHOLD = 4000;
+ * const int TRAY_DIST_CM = 20;
+ * const int FAIL_TIMEOUT = 5000;
+ * 
+ * Tasks:
+ * task monitorTray()
+ * task sharpenAndSort()
+ * task main()
+ *
+ * @author: Eugene Wang, Feilan Jiang, Kenta Morris, Felix Cheng */
 
 #include "actions.c"
 #include "controls.c"
 #include "util.c"
 #include "ports.c"
 
-const int BATTERY_THRESHOLD = 0;	 //battery level for runtime operation
-const int TRAY_DIST_CM = 20;  //threshold hole level for normal tray to sensorSonar dist
-const int FAIL_TIMEOUT = 5000; // in a failed state, how long robot will wait for
-							   // user input before continuing
+const int BATTERY_THRESHOLD = 4000; // mininum battery level (in mV) for runtime operation
+const int TRAY_DIST_CM = 20; // distance between each colour bin
+const int FAIL_TIMEOUT = 5000; // in a failed state, how long the robot will wait for
+			       // user input before continuing
 
-/**  monitorTray
- * Check that the pencil tray is not removed during sharpening.
- * @return a void ?task? do not return anything.
-*/
+/** monitorTray
+ * Check that the pencil tray is not removed during sharpening. If the tray is removed,
+ * all other tasks will be stopped until it is restored. */
 task monitorTray() {
-	while (nAvgBatteryLevel > BATTERY_THRESHOLD) //have battery
+	while (nAvgBatteryLevel > BATTERY_THRESHOLD)
 	{
-		if (SensorValue[ULTRA] > TRAY_DIST_CM) 	//tray out of bound
+                // ultrasonic sensor reading will be greater than |TRAY_DIST_CM| when the
+                // tray is gone
+		if (SensorValue[ULTRA] > TRAY_DIST_CM)
 		{
 			pauseMotors();
 			playSound(soundException);
@@ -33,12 +41,11 @@ task monitorTray() {
 			displayString(4, "Put back to resume");
 			displayString(5, "sharpening.");
 
-			// pause all program execution until tray replaced
+			// stop all other tasks
 			hogCPU();
-			while (SensorValue[ULTRA] > TRAY_DIST_CM) { } 	//continue action when tray out of bound
-			
-			//tray within bound: 
-			
+			while (SensorValue[ULTRA] > TRAY_DIST_CM) { }
+
+                        // resume other tasks when the tray is restored
 			releaseCPU();
 			resumeMotors();
 			eraseDisplay();
@@ -46,31 +53,29 @@ task monitorTray() {
 	}
 }
  
-/**  sharpenAndSort 
- * the program is the core of the sharpenning and sorting feature
- * it will handle the operation loop that ultimately handles whether
- * the program will transport the pencil, sort, or stops
- *
- * @return a void task do not return anything. */
-
+/** sharpenAndSort 
+ * This task represents the main purpose of the robot. It contains the operation
+ * loop that ultimately handles whether the program will sharpen pencils, sort them, 
+ * or stop altogether. After all pencils are sorted, the program displays
+ * an inventory count of sharpened pencils. From here, the user can choose to quit the
+ * program or sharpen more pencils. */
 task sharpenAndSort() {
-	bool quit = false; //user quits
+	bool quit = false;
 	bool finishedSharpening = false; 
-	int color = 0;	//current pencil color
-	int count[N_BINS] = { 0, 0, 0, 0, 0, 0, 0 };// color array
-	Status stat;	//status or operation
+	int color = 0;
+	int count[N_BINS] = { 0, 0, 0, 0, 0, 0, 0 }; // histogram of pencil colours
+	Status stat; // whether each action function completed successfully
 
 	do {
-		while (!finishedSharpening) 	//inoperation
+		while (!finishedSharpening)
 		{
-			stat = feedPencil();	//feedpencil
+			stat = feedPencil();
 
 			if (stat == SUCCESS) {
-				//detect color, and record
 				color = getPencilColor();
-				count[color]++;			
-				
-				//if failed to detect
+				count[color]++;	
+
+				// a value of 0 represents an invalid colour
 				if (color == 0) {
 					displayString(0, "Color detection");
 					displayString(1, "failed");
@@ -81,99 +86,91 @@ task sharpenAndSort() {
 					eraseDisplay();
 				}
 
-				do {
-					stat = alignSharpener();	//move sharpener in place
+                                stat = alignSharpener();
+                                if (stat == JAMMED) {
+                                        displayString(0, "Possible jam in");
+                                        displayString(1, "tray motor");
+                                        displayString(3, "Press C to resume");
+                                        displayString(4, "operation");
+                                        waitForBtnPress(CENTER_BTN);
+                                        eraseDisplay();
+                                } else if (stat == TIMED_OUT) {
+                                        displayString(0, "Possible derailment");
+                                        displayString(1, "of tray");
+                                        displayString(3, "Press C to resume");
+                                        displayString(4, "operation");
+                                        waitForBtnPress(CENTER_BTN);
+                                        eraseDisplay();
+                                }
 
-					if (stat == JAMMED) {		//if tray jamed
-						displayString(0, "Possible jam in");
-						displayString(1, "tray motor");
-						displayString(3, "Press C to resume");
-						displayString(4, "operation");
-						waitForBtnPress(CENTER_BTN);
-						eraseDisplay();
-					} else if (stat == TIMED_OUT) {	//if time out before reach
-						displayString(0, "Possible derailment");
-						displayString(1, "of tray");
-						displayString(3, "Press C to resume");
-						displayString(4, "operation");
-						waitForBtnPress(CENTER_BTN);
-						eraseDisplay();
-					}
-				} while (stat != SUCCESS);	//continue util success status is reached
+                                stat = sharpenPencil();
+                                if (stat == JAMMED) {
+                                        displayString(0, "Possible jam in");
+                                        displayString(1, "cartridge");
+                                        displayString(3, "Press C to resume");
+                                        displayString(4, "operation");
+                                        waitForBtnPress(CENTER_BTN);
+                                        eraseDisplay();
+                                } else if (stat == TIMED_OUT) {
+                                        spinWheels(-100, 4000);
 
-				do {
-					stat = sharpenPencil();	//sharpen it
-					if (stat == JAMMED) {
-						displayString(0, "Possible jam in");
-						displayString(1, "cartridge");
-						displayString(3, "Press C to resume");
-						displayString(4, "operation");
-						waitForBtnPress(CENTER_BTN);
-						eraseDisplay();
-					} else if (stat == TIMED_OUT) {
-						spinWheels(-100, 4000);
+                                        if (SensorValue[WHEEL_TOUCH]) {
+                                                displayString(0, "Pencil possibly");
+                                                displayString(1, "stuck in");
+                                                displayString(2, "sharpener");
+                                                displayString(4, "Press C to resume");
+                                                displayString(5, "operation");
+                                                waitForBtnPress(CENTER_BTN);
+                                                eraseDisplay();
+                                        }
+                                }
 
-						if (SensorValue[WHEEL_TOUCH]) {
-							displayString(0, "Pencil possibly");
-							displayString(1, "stuck in");
-							displayString(2, "sharpener");
-							displayString(4, "Press C to resume");
-							displayString(5, "operation");
-							waitForBtnPress(CENTER_BTN);
-							eraseDisplay();
-						}
-					}
-				} while (stat == JAMMED);
+                                stat = moveTrayToColor(color);
+                                if (stat == JAMMED) {
+                                        displayString(0, "Possible jam in");
+                                        displayString(1, "tray motor");
+                                        displayString(3, "Press C to resume");
+                                        displayString(4, "operation");
+                                        waitForBtnPress(CENTER_BTN);
+                                        eraseDisplay();
+                                } else if (stat == TIMED_OUT) {
+                                        displayString(0, "Possible derailment");
+                                        displayString(1, "of tray");
+                                        displayString(3, "Press C to resume");
+                                        displayString(4, "operation");
+                                        waitForBtnPress(CENTER_BTN);
+                                        eraseDisplay();
+                                }
 
-				do {
-					stat = moveTrayToColor(color);	//move tray to certain color
-					if (stat == JAMMED) {
-						displayString(0, "Possible jam in");
-						displayString(1, "tray motor");
-						displayString(3, "Press C to resume");
-						displayString(4, "operation");
-						waitForBtnPress(CENTER_BTN);
-						eraseDisplay();
-					} else if (stat == TIMED_OUT) {
-						displayString(0, "Possible derailment");
-						displayString(1, "of tray");
-						displayString(3, "Press C to resume");
-						displayString(4, "operation");
-						waitForBtnPress(CENTER_BTN);
-						eraseDisplay();
-					}
-				} while (stat != SUCCESS);
+                                stat = ejectPencil();
+                                if (stat == JAMMED) {
+                                        displayString(0, "Pencil possibly");
+                                        displayString(1, "stuck in");
+                                        displayString(2, "cartridge");
+                                        displayString(4, "Press C to resume");
+                                        displayString(5, "operation");
+                                        waitForBtnPress(CENTER_BTN);
+                                        eraseDisplay();
+                                } else if (stat == TIMED_OUT) {
+                                        spinWheels(100, 4000);
 
-				do {
-					stat = ejectPencil();		//eject pencil
-					if (stat == JAMMED) {
-						displayString(0, "Pencil possibly");
-						displayString(1, "stuck in");
-						displayString(2, "cartridge");
-						displayString(4, "Press C to resume");
-						displayString(5, "operation");
-						waitForBtnPress(CENTER_BTN);
-						eraseDisplay();
-					} else if (stat == TIMED_OUT) {
-						spinWheels(100, 4000);
-
-						if (SensorValue[WHEEL_TOUCH]) {
-							displayString(0, "Pencil possibly");
-							displayString(1, "hanging on");
-							displayString(2, "cartridge");
-							displayString(4, "Press C to resume");
-							displayString(5, "operation");
-							waitForBtnPress(CENTER_BTN);
-							eraseDisplay();
-						}
-					}
-				} while (stat == JAMMED);
+                                        if (SensorValue[WHEEL_TOUCH]) {
+                                                displayString(0, "Pencil possibly");
+                                                displayString(1, "hanging on");
+                                                displayString(2, "cartridge");
+                                                displayString(4, "Press C to resume");
+                                                displayString(5, "operation");
+                                                waitForBtnPress(CENTER_BTN);
+                                                eraseDisplay();
+                                        }
+                                }
 			} else {
-				finishedSharpening = true;		//finish one round
+                                // sharpening is complete for the current _batch_ of pencils
+				finishedSharpening = true;
 			}
 		}
 
-		quit = displayEndScreen(count, 0); //display result and prompt for continuation or termination
+		quit = displayEndScreen(count, 0); // display pencil inventory and prompt user to continue or quit the program
 	} while (!quit);
 
 	stopAllTasks();
@@ -181,12 +178,11 @@ task sharpenAndSort() {
 }
 
 /** main
- * the main task of the program, where all the higher lever functions
- * were called
- * @return a void task do not return anything. */
+ * The main task of the program. Sensor initialization and task spawning occur here. */
 task main() {
 	init();
 	promptStart();
 	startTask(monitorTray);
 	startTask(sharpenAndSort);
+	while (nAvgBatteryLevel > BATTERY_THRESHOLD) { }
 }
