ecea05747f02882502181c42222d318c0408090e
Feilan Jiang <f-jiang@github.com>
2016-12-04 23:43:32 -0500

    Revise comments in actions.c

--- code/actions.c
+++ code/actions.c
@@ -1,27 +1,21 @@
 /**  actions.c
-* contains the action command of the Csharp program.
-* handle the moving pencils, sharpening pencil, 
-* ejecting pencils, and moving trays. Essentially, the basic component of
-* the machine, it includes these classes, variables, and functions: </p>
-*
-* defined Enum "status"
-*
-* Declared follow functions: 
-* void promptStart() 
-* bool displayEndScreen(int *histogram, int colorOrder) 
-* Status feedPencil(int timeout = 5000)
-* Status sharpenPencil(int sharpenDuration = 3000, int timeout = 5000)
-* Status alignSharpener(int timeout = 7000)
-* Status ejectPencil(int timeout = 7000)
-* Status moveTrayToColor(int color)
-* int getPencilColor(int tMs = 1000)
-* 
-* <b>Note:</b> The program will only work when all the specified files,
-* function, and variable are present in the package
-*
-* @author: Euene Wang, Feilan Jiang, Kenta Morris, Felix Cheng
-* @version 1.2
-* @since   2016-11-23   */
+ * Contains the action commands of the Csharp program.
+ * These functions represent the different tasks performed by the robot
+ * such as moving, sharpening, and ejecting pencils as well as moving the tray.
+ * It includes the following contents:
+ *
+ * Enumerated types:
+ * Status
+ *
+ * Functions: 
+ * void promptStart() 
+ * bool displayEndScreen(int *histogram, int colorOrder) 
+ * Status feedPencil(int timeout = 3000)
+ * Status sharpenPencil(int sharpenDuration = 3000, int timeout = 5000)
+ * Status alignSharpener(int timeout = 7000)
+ * Status ejectPencil(int timeout = 5000)
+ * Status moveTrayToColor(int color)
+ * int getPencilColor(int samplingDuration = 1000) */
 
 #ifndef ACTIONS_C
 #define ACTIONS_C
@@ -30,41 +24,39 @@
 #include "ports.c"
 #include "util.c"
 
-//enum status for functions to return starts datatype
 typedef enum status_t { SUCCESS, JAMMED, TIMED_OUT } Status; 
 
 
-/**  monitorTray
- * display strings, and wait for user button press to start
- * the program  */
+/** monitorTray
+ * 
+ * Prompts user to press the start button, after which the sharpening of pencils will begin. */
 void promptStart() {
 	displayString(3, "Press centre btn");
 	displayString(4, "to start");
 	waitForBtnPress(CENTER_BTN);
 }
 
-/**  displayEndScreen
- * It handles the display of sorted contain in the program
- * first it display a list of sorted pencil depending on users' requirement, 
- * sort the pencils in different ways: assending, desecnding, and by color index
+/** displayEndScreen
+ * Displays a inventory list of sharpened pencils which are categorized by colour.
+ * Users are able to cycle through the following list sort orders:
+ * - Ascending order with respect to pencil count
+ * - Descending order with respect to pencil count
+ * - Default colour order as defined by RobotC
  * 
- * @param histogram int array pointer for the array to be sorted
- * @param colorOrder for the order of which the pencil will be sorted 
- * @return bool: whether or not the programme shall continue  */
+ * @param histogram: int array containing the pencil count for each colour category
+ * @param colorOrder: identifier for which of the above sort orders to use
+ * @return bool: whether or not the program shall continue */
 bool displayEndScreen(int *histogram, int colorOrder) {
-	//color array
+	// array of colour labels that is parallel to |histogram|
 	string penColors[N_BINS] = {"invalid:%8d" , "black:  %8d", "blue:   %8d",
 	"green:  %8d", "yellow: %8d", "red:    %8d", "white:  %8d" };
 	
-	//display first sorted list
 	displayString(0, "LQt,CShrp,RSrt");
-	if (colorOrder == 0){
-		for (int i = 0; i<N_BINS; i++){
+	if (colorOrder == 0) {
+		for (int i = 0; i<N_BINS; i++) {
 			displayString(i + 1, penColors[i], histogram[i]);
 		}
-	}
-	else		//switch to other options based on users' spec
-	{
+	} else {
 		int tbSorted [N_BINS];
 		string colors[N_BINS];
 		bool notTaken[N_BINS];
@@ -82,7 +74,7 @@ bool displayEndScreen(int *histogram, int colorOrder) {
 		}
 		
 		
-		//assign sorted array to the correct color that each value correspond to
+		// rearrange |penColors| to make it parallel with the newly sorted |histogram|
 		for (int x = 0; x < N_BINS; x ++){
 			for (int y = 0; y < N_BINS; y ++){
 				if (tbSorted[x] == histogram[y] && notTaken[x]) {
@@ -92,16 +84,16 @@ bool displayEndScreen(int *histogram, int colorOrder) {
 			}
 		}
 		
-		//display sorted result
+		// display sorted result
 		for (int i = 0; i <N_BINS; i++){
 			displayString (i + 1, colors[i], tbSorted[i]);
 		}
 	}
 
-	//prompt user for input after each sort
+	// prompt user for input
 	int a = waitForBtnPress();
 
-	//decide the next task based on user input
+	// determine the next task based on user input
 	if (a == LEFT_BTN) {
 		eraseDisplay();
 		return true;
@@ -115,17 +107,22 @@ bool displayEndScreen(int *histogram, int colorOrder) {
 	}
 }
 
-/**  feedPencil
- * this function handls spinning wheels to feed the pencil into the sharpener
- * it takes in a parameter times out to determine whether there are more pencils
- * left in the bin
- * 
- * @param timesout: no more pencils
- * @return jammed: pencils stuck in cartridge   */
+/** feedPencil
+ * This function uses the wheels to feed the pencil into the cartridge exit hole.
+ * The function exits when |timeout| is exceeded or when the exit hole touch sensor
+ * is pressed, whichever happens first.
+ * A |Status| value is returned to indicate the occurrence of errors.
+ *
+ * @param timeout: duration (in ms) for which the program will attempt to retract the pencil;
+                   a status of TIMED_OUT will be returned if |timeOut| is exceeded
+ * @return Status:
+ *     SUCCESS: action completed successfully
+ *     JAMMED: pencil jam in cartridge
+ *     TIMED_OUT: pencil fell off conveyor belt */
 Status feedPencil(int timeout = 3000) 
 {
-	// |moveBelt()|: get pencil to wheels
-	// |spinWheels()|: get pencil into sharpening-ready position (i.e. right past touch sensor)
+	// |moveBelt()|: move pencil to wheels
+	// |spinWheels()|: move pencil to sharpening-ready position (i.e. right past touch sensor)
 	if (moveBelt(50, 500) && spinWheels(50)) {
 		long t = time1[T1];
 		while (!SensorValue[WHEEL_TOUCH] && time1[T1] - t < timeout) { }
@@ -142,18 +139,19 @@ Status feedPencil(int timeout = 3000)
 	}
 }
 
-
-/**  sharpenPencil
- * spin the wheels to push the pencils into the sharpener and retract it after it is finished.
+/** sharpenPencil
+ * Feed the pencil into the sharpener hole and retract it after |sharpenDuration| is exceeded. 
+ * A |Status| value is returned to indicate the occurrence of errors.
  * 
- * @param timesout: time until it returns a timeOut status
- * @param sharpenPencil: duration for shapening the pencil
- * @return status
- *		JAMMED: pencils stuck in cartridge
- *		TIMED_OUT: pencils stuck in sharpener
- *		SUCCESS: task completed                 */
+ * @param sharpenDuration: duration (in ms) for which the pencil will be sharpened
+ * @param timeout: duration (in ms) for which the program will attempt to retract the pencil;
+                   a status of |TIMED_OUT| will be returned if |timeOut| is exceeded
+ * @return Status:
+ *     SUCCESS: action completed successfully
+ *     JAMMED: pencil jam at cartridge exit hole
+ *     TIMED_OUT: pencil is stuck in sharpener hole */
 Status sharpenPencil(int sharpenDuration = 3000, int timeout = 5000) {
-	if (spinWheels(50, 1000)) { // push pencil into sharpener
+	if (spinWheels(50, 1000)) { // push pencil into sharpener hole
 		long t;
 		spinWheels(25, sharpenDuration); // wait for pencil to be sharpened
 
@@ -168,23 +166,27 @@ Status sharpenPencil(int sharpenDuration = 3000, int timeout = 5000) {
 	}
 }
 
-/**  alignSharpener
- * This function aligns the sharpener to the slider and feeder port
- * essentially after the tray was moved to a set location for dropping
- * the pencil, this function resets the tray and sharpener to its 
- * initial position
+/** alignSharpener
+ * This function aligns the sharpener with the cartridge exit hole.
+ * It is used by the program as a way of "zeroing" the sliding tray mechamism.
+ * A |Status| value is returned to indicate the occurrence of errors.
  * 
- * @param timesout: time until it returns a timeOut status
- * @return status
- *		JAMMED: object in way
- *		TIMED_OUT: derailed tray 	*/
-Status alignSharpener(bool dir = true, int timeout = 3000){
-	if (moveTray(dir ? -25 : 25)) {
+ * @param travel: indicates the direction in which the tray must travel
+ * @param timeout: duration (in ms) for which the program will attempt to retract the pencil;
+                   a status of |TIMED_OUT| will be returned if |timeOut| is exceeded
+ * @return Status:
+ *     SUCCESS: action completed successfully
+ *     JAMMED: tray motor is jammed
+ *     TIMED_OUT: tray derailment */
+Status alignSharpener(bool travel = true, int timeout = 3000) {
+	if (moveTray(travel ? -25 : 25)) {
 		long t = time1[T1];
-		if (dir) 
-			while(SensorValue[TRAY_TOUCH] && time1[T1] - t < timeout) { }
-		else
-			while(!(SensorValue[TRAY_TOUCH] && time1[T1] - t < timeout)) { }
+		if (travel) { 
+			while (SensorValue[TRAY_TOUCH] && time1[T1] - t < timeout) { }
+		} else {
+			while (!(SensorValue[TRAY_TOUCH] && time1[T1] - t < timeout)) { }
+                }
+
 		moveTray(0);
 		return (time1[T1] - t < timeout) ? SUCCESS : TIMED_OUT;
 	} else {
@@ -192,49 +194,78 @@ Status alignSharpener(bool dir = true, int timeout = 3000){
 	}
 }
 
-/**  ejectPencil
- * ejects the pencil from the feeder and check for environmental status
+/** ejectPencil
+ * Ejects the pencil from the cartridge.
+ * A |Status| value is returned to indicate the occurrence of errors.
  * 
- * @param timesout: time until it returns a timeOut status
- * @return status
- *		JAMMED: pencil stuck in feeder
- *		TIMED_OUT: pencil hanging      */
-Status ejectPencil(int timeout = 5000){
-	if (spinWheels(50)){
+ * @param timeout: duration (in ms) for which the program will attempt to eject the pencil;
+                   a status of TIMED_OUT will be returned if |timeOut| is exceeded
+ * @return Status:
+ *     SUCCESS: action completed successfully
+ *     JAMMED: pencil is stuck in cartridge exit hole
+ *     TIMED_OUT: pencil is dangling in the exit hole */
+Status ejectPencil(int timeout = 5000) {
+	if (spinWheels(50)) {
 		long t = time1[T1];
 		// pencil begins behind touch sensor
-		while(!SensorValue[WHEEL_TOUCH] && time1[T1] - t < timeout ){}
-		// keep pushing till pencil past touch sensor
-		while(SensorValue[WHEEL_TOUCH] && time1[T1] - t < timeout){}
-		// push for an additional number of encoder counts in order to eject pencil
+		while(!SensorValue[WHEEL_TOUCH] && time1[T1] - t < timeout ) { }
+		// keep pushing until pencil past touch sensor
+		while(SensorValue[WHEEL_TOUCH] && time1[T1] - t < timeout) { }
+
+                // spin wheels for a full revolution to ensure pencil is properly ejected
 		nMotorEncoder[WHEEL] = 0;
-		while(abs(nMotorEncoder[WHEEL]) < 360 && time1[T1] - t < timeout){}//change as needed
+		while(abs(nMotorEncoder[WHEEL]) < 360 && time1[T1] - t < timeout) { }
 
 		spinWheels(0);
 
-		return (time1[T1] - t < timeout)? SUCCESS : TIMED_OUT;
+		return (time1[T1] - t < timeout) ? SUCCESS : TIMED_OUT;
 	} else {
 		return JAMMED;
 	}
 }
 
-/**  getPencilColor
- * This function detect pencils ambiant color, calculate pencils' 
- * average color and then returns it as the pencils' color
+/** moveTrayToColor
+ * This function moves the tray to the bin that corresponds to the parameter |color|.
+ * This is to ensure that the pencil is dropped in the correct bin.
+ * A |Status| value is returned to indicate the occurrence of errors.
  * 
- * @param timesout: time until it returns a timeOut status
- * @return color index ( 0 == invalid color)  */
-int getPencilColor(int tMs = 1000) {
+ * @param timeout: duration (in ms) for which the program will attempt to move the tray;
+                   a status of |TIMED_OUT| will be returned if |timeOut| is exceeded
+ * @return Status:
+ *     SUCCESS: action completed successfully
+ *     JAMMED: tray motor is jammed
+ *     TIMED_OUT: tray derailment */
+Status moveTrayToColor(int color) {
+       Status status = alignSharpener(false);
+
+       if (status == SUCCESS) {
+               if (moveTray(25, (color + 1) * BIN_DIST_CM)) {
+                  return SUCCESS;
+               } else {
+                  return JAMMED;
+            }
+       }
+
+       return status;
+}
+
+/** getPencilColor
+ * This function determines the pencil's colour by obtaining a large sample of readings
+ * and returning the mode colour value.
+ *
+ * @param samplingDuration: the duration (in ms) for which to obtain colour samples
+ * @return int: the mode colour value obtained from the samples */
+int getPencilColor(int samplingDuration = 1000) {
 	long t;
 
 	t = time1[T1];
 	int colorType[N_BINS] = { 0, 0, 0, 0, 0, 0, 0 };
-	while(time1[T1] - t < tMs){//get average colour seen
+	while (time1[T1] - t < samplingDuration) { // get average colour seen
 		colorType[SensorValue[COLOR]]++;
 	}
 
 	int modeColorFreq = 0, modeColor = 0;
-	for (int i = 0; i < N_BINS; i++){//finds colour sensed the most frequently
+	for (int i = 0; i < N_BINS; i++){ // finds colour sensed the most frequently
 		if (colorType[i] > modeColorFreq){
 			modeColorFreq = colorType[i];
 			modeColor = i;
@@ -244,24 +275,4 @@ int getPencilColor(int tMs = 1000) {
 	return modeColor;
 }
 
-/**  moveTrayToColor
- * This function moves the tray to a certain color at int color.
- * it allows the pencil to be dropped to the correct location
- * and return runtime status for error handling on a higher level
- * 
- * @param timesout: time until it returns a timeOut status
- * @return status
- *		JAMMED: object in way
- *		TIMED_OUT: derailed tray
-*/
-Status moveTrayToColor(int color) {
-       Status tempStatus = alignSharpener(false);
-       if (tempStatus == SUCCESS){
-               if (moveTray(25, (color + 1) * BIN_DIST_CM))
-                  return SUCCESS;
-               else
-                 	return JAMMED;
-       }
-       return tempStatus;
- }
 #endif
